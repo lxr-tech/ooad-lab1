b6d27858a529297f65804262105a343da01b8958
lxr-tech <1838593642@qq.com>
2022-10-14 12:08:29 +0800

    add interface

--- test-221004a.py
+++ test-221004a.py
@@ -2,77 +2,118 @@ import sys
 import argparse
 
 
-def add_title():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-add_title", help="add sth.")
-    parser.add_argument("-at", help="at sp.")
-    args = parser.parse_args()
-    print(f"this is an add_title function, info: {{add: {args.add_title}, at: {args.at}, }}")
+class AddInterface(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-add", help="add sth.")
+        parser.add_argument("-at", help="at sp.")
+        args = parser.parse_args()
+        print(f"this is an add_title function, info: {{add: {args.add_title}, at: {args.at}, }}")
 
 
-def add_bookmark():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-add_bookmark", help="add sth.")
-    parser.add_argument("-at", help="at sp.")
-    args = parser.parse_args()
-    print(f"this is an add_bookmark function, info: {{add: {args.add_bookmark}, at: {args.at}, }}")
+class DeleteInterface(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def delete():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-delete", help="delete sth.")
+        args = parser.parse_args()
+        print(f"this is a delete function, info: {{delete: {args.delete}, }}")
 
 
-def delete():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-delete", help="delete sth.")
-    args = parser.parse_args()
-    print(f"this is a delete function, info: {{delete: {args.delete}, }}")
+class OpenInterface(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def open():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-open", help="open sth.")
+        args = parser.parse_args()
+        print(f"this is an open function, info: {{open: {args.open}, }}")
 
 
-def open_():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-open", help="open sth.")
-    args = parser.parse_args()
-    print(f"this is an open function, info: {{open: {args.open}, }}")
+class SaveInterface(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def save():
+        print(f'this is a save function, info: {{}}')
 
 
-def save():
-    print(f'this is a save function, info: {{}}')
+class UndoInterface(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def undo():
+        print(f'this is an undo function, info: {{}}')
 
 
-def undo():
-    print(f'this is an undo function, info: {{}}')
+class RedoInterface(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def redo():
+        print(f'this is a redo function, info: {{}}')
 
 
-def redo():
-    print(f'this is a redo function, info: {{}}')
+class ShowTreeInterface(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def show_tree():
+        print(f'this is a show_tree function, info: {{}}')
 
 
-def show_tree():
-    print(f'this is a show_tree function, info: {{}}')
+class LsTreeInterface(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def ls_tree():
+        print(f'this is a ls_tree function, info: {{}}')
 
 
-def ls_tree():
-    print(f'this is a ls_tree function, info: {{}}')
+class ReadBookmarkInterface(object):
+    def __init__(self):
+        pass
 
-
-def read_bookmark():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-read_bookmark", help="read sth.")
-    args = parser.parse_args()
-    print(f'this is a read_bookmark function, info: {{read: {args.read_bookmark}, }}')
+    @staticmethod
+    def read_bookmark():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-read_bookmark", help="read sth.")
+        args = parser.parse_args()
+        print(f'this is a read_bookmark function, info: {{read: {args.read_bookmark}, }}')
 
 
 def undefined():
     print('this is an undefined function')
 
 
-# todo: add (add-title [at], add-bookmark [at], )
-#       delete (delete-title, delete-bookmark, )
-#       open, bookmark, edit
-#       save, undo, redo
+# todo: add (add-title [at], add-bookmark [at], ),
+#       delete (delete-title, delete-bookmark, ),
+#       open, bookmark, edit,
+#       save, undo, redo,
 #       show-tree, ls-tree,
 #       read-bookmark
 
-func_dict = {'-add_title': add_title, '-add_bookmark': add_bookmark, '-delete': delete,
-             '-open': open_, '-save': save, '-undo': undo, '-redo': redo,
-             '-show_tree': show_tree, '-ls_tree': ls_tree, '-read_bookmark': read_bookmark, }
+func_dict = {'-add': AddInterface.add,
+             '-delete': DeleteInterface.delete,
+             '-open': OpenInterface.open, '-save': SaveInterface.save,
+             '-undo': UndoInterface.undo, '-redo': RedoInterface.redo,
+             '-show_tree': ShowTreeInterface.show_tree,
+             '-ls_tree': LsTreeInterface.ls_tree,
+             '-read_bookmark': ReadBookmarkInterface.read_bookmark, }
 
 
 def get_input():
